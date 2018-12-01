a03cc1a779a09c68747ed390c9df832d61109944
tru <trupti.patekar@springernature.com>
2018-12-01 09:13:46 +0000

    1

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 root:x:0:0:root:/root:/bin/bash
-daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+#daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
 bin:x:2:2:bin:/bin:/usr/sbin/nologin
 sys:x:3:3:sys:/dev:/usr/sbin/nologin
 sync:x:4:65534:sync:/bin:/bin/sync
